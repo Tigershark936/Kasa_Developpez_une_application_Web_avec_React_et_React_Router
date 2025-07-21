fa4a92709015d2eedf57a6ba74505b9964610a12
Alain Daly <alain.daly.93600@gmail.com>
2025-07-21 15:39:14 +0200

    feat: create Card component with container and SCSS styles

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,1 +1,13 @@
 import styles from './Card.module.scss';
+
+function Card(){
+    return (
+        <div className={styles.container}>
+            <div className={styles.card}>
+                <h1 className={styles.h1}>Titre de la location</h1>
+            </div>
+        </div>
+    )
+}
+
+export default Card
--- src/pages/Home/Home.jsx
+++ src/pages/Home/Home.jsx
@@ -1,11 +1,13 @@
 import Banner from "../../components/Banner/Banner";
+import Card from "../../components/Card/Card";
 
 function Home() {
   return (
     <>
         <Banner/>
+        <Card/>
     </>
-  );
+  )
 }
 
 export default Home;
