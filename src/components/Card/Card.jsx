7fb0c85949f7eaa8d2b207fcaa1432bde6586a99
Alain Daly <alain.daly.93600@gmail.com>
2025-07-21 17:14:11 +0200

    feat: map housing data to display dynamic Card components

--- src/components/Card/Card.jsx
+++ src/components/Card/Card.jsx
@@ -1,11 +1,25 @@
+import { Link } from 'react-router-dom';
+import { DescriptionHousing } from '../../data/DescriptionHousing';
 import styles from './Card.module.scss';
 
 function Card(){
+    // On crée un tableau de JSX avec .map()
+    const announces = DescriptionHousing.map((announce) => {
+        
+        return (
+        <div key={announce.id} className={styles.cardContainer}>
+            <Link  to='/' className={styles.card}>
+                <img src={announce.cover} alt='Picture logement' className={styles.img}/>
+                <h1 className={styles.h1}>{announce.title}</h1>
+            </Link>
+        </div>
+    )
+    });
+    
+    // On affiche le tableau dans le JSX
     return (
         <div className={styles.container}>
-            <div className={styles.card}>
-                <h1 className={styles.h1}>Titre de la location</h1>
-            </div>
+            {announces}
         </div>
     )
 }
