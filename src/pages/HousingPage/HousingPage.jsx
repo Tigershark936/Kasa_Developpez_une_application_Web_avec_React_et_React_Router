13050ae2f68db10dd5aa6a5ddca641cce9bb4c0e
Alain Daly <alain.daly.93600@gmail.com>
2025-07-28 15:20:01 +0200

    feat: add Slideshow with dynamic navigation (handlePrevious & handleNext) + conditional display of arrows and numbering

--- src/pages/HousingPage/HousingPage.jsx
+++ src/pages/HousingPage/HousingPage.jsx
@@ -1,9 +1,28 @@
 import { DescriptionHousing } from "../../data/DescriptionHousing";
 import Slideshow from "../../components/Slideshow/Slideshow";
+import { useParams } from "react-router-dom";
+import NotFound from '../NotFound/NotFound';
+
 
 const HousingPage = () => {
+    // 1.Récupérer l'ID depuis l'URL
+    const { id } = useParams();
+
+    // 2.Chercher le logement dans la data
+    const selectedHousing = DescriptionHousing.find((housing) => housing.id === id);
+    
+    // 3.Si le logement n’existe pas → afficher NotFound
+    if(!selectedHousing){
+        return <NotFound />;
+    }
+
+    // 4.Si le logement existe → j'accéder à ses données
+    const pictures = selectedHousing.pictures;
+
     return (
-        < Slideshow />
+        < Slideshow 
+            pictures={pictures}
+        />
     )
 }
 
